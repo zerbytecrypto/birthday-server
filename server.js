ec5e5cab7cff1bb07ce0e4c99bd5311bfe08ae01
zerbytecrypto <zerbytesintercambio@gmail.com>
2026-05-30 03:50:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,57 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function getZodiacSign(day, month) {
+
+    if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) {
+        return "Aquarius";
+    }
+
+    if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+        return "Pisces";
+    }
+
+    if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
+        return "Aries";
+    }
+
+    if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) {
+        return "Taurus";
+    }
+
+    if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+        return "Gemini";
+    }
+
+    if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) {
+        return "Cancer";
+    }
+
+    if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
+        return "Leo";
+    }
+
+    if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
+        return "Virgo";
+    }
+
+    if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
+        return "Libra";
+    }
+
+    if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
+        return "Scorpio";
+    }
+
+    if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
+        return "Sagittarius";
+    }
+
+    return "Capricorn";
+}
+
 function getBirthdayData(dob) {
+
     const now = new Date();
     const birthDate = new Date(dob);
 
@@ -26,7 +76,7 @@ function getBirthdayData(dob) {
         age--;
     }
 
-    // Time difference
+    // Time lived
     const diff = now - birthDate;
 
     const seconds = Math.floor(diff / 1000);
@@ -34,7 +84,14 @@ function getBirthdayData(dob) {
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
 
+    // Zodiac
+    const zodiac = getZodiacSign(
+        birthDate.getDate(),
+        birthDate.getMonth() + 1
+    );
+
     return {
+        zodiac,
         age,
         daysLived: days,
         hoursLived: hours,
@@ -44,6 +101,7 @@ function getBirthdayData(dob) {
 }
 
 app.post("/birthday", (req, res) => {
+
     const { dob } = req.body;
 
     if (!dob) {
